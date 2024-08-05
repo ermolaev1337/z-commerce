3700cbe7447b0b5609461486ef43996761f6ba24
Egor Ermolaev <egor.ermolaev@uni.lu>
2024-08-05 19:40:08 +0200

    fix(cred): mocked cred view/modal

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ docker stop $(docker ps -qa)
 docker rm $(docker ps -qa)
 
 
-docker-compose -f ./z-commerce-medusa/docker-compose.yml build
-docker-compose -f ./z-commerce-medusa/docker-compose.yml up -d
+docker compose -f ./z-commerce-medusa/docker-compose.yml build
+docker compose -f ./z-commerce-medusa/docker-compose.yml up -d
 until [ "`docker inspect -f {{.State.Health.Status}} db`"=="healthy" ]; do
     sleep 0.1;
 done;
@@ -16,43 +16,43 @@ done;
 echo Medusa Backend is healthy and running
 
 
-docker-compose -f ./z-commerce-storefront/docker-compose.yml build
-docker-compose -f ./z-commerce-storefront/docker-compose.yml up -d
+docker compose -f ./z-commerce-storefront/docker-compose.yml build
+docker compose -f ./z-commerce-storefront/docker-compose.yml up -d
 until [ "`docker inspect -f {{.State.Health.Status}} storefront`"=="healthy" ]; do
     sleep 0.1;
 done;
 echo Medusa Frontend is healthy and running
 
-docker-compose -f ./z-commerce-socket/docker-compose.yml build
-docker-compose -f ./z-commerce-socket/docker-compose.yml up -d
+docker compose -f ./z-commerce-socket/docker-compose.yml build
+docker compose -f ./z-commerce-socket/docker-compose.yml up -d
 until [ "`docker inspect -f {{.State.Health.Status}} socket`"=="healthy" ]; do
     sleep 0.1;
 done;
 echo Medusa Socket is healthy and running
 
-docker-compose -f ./z-commerce-controller/docker-compose.yml build
-docker-compose -f ./z-commerce-controller/docker-compose.yml up -d
+docker compose -f ./z-commerce-controller/docker-compose.yml build
+docker compose -f ./z-commerce-controller/docker-compose.yml up -d
 until [ "`docker inspect -f {{.State.Health.Status}} controller`"=="healthy" ]; do
     sleep 0.1;
 done;
 echo Controller Backend is healthy and running
 
-docker-compose -f ./z-commerce-wallet/docker-compose.yml build
-docker-compose -f ./z-commerce-wallet/docker-compose.yml up -d
+docker compose -f ./z-commerce-wallet/docker-compose.yml build
+docker compose -f ./z-commerce-wallet/docker-compose.yml up -d
 until [ "`docker inspect -f {{.State.Health.Status}} wallet-backend`"=="healthy" ]; do
     sleep 0.1;
 done;
 echo Wallet Backend is healthy and running. Wallet Frontend has started
 
-docker-compose -f ./z-commerce-heimdall/docker-compose.yml build
-docker-compose -f ./z-commerce-heimdall/docker-compose.yml up -d
+docker compose -f ./z-commerce-heimdall/docker-compose.yml build
+docker compose -f ./z-commerce-heimdall/docker-compose.yml up -d
 #until [ "`docker inspect -f {{.State.Health.Status}} bootstrap`"=="socket" ]; do
 #    sleep 0.1;
 #done;
 echo Heimdall\'s Bootstrap Backend has finished
 
-docker-compose -f ./z-commerce-delivery/docker-compose.yml build
-docker-compose -f ./z-commerce-delivery/docker-compose.yml up -d
+docker compose -f ./z-commerce-delivery/docker-compose.yml build
+docker compose -f ./z-commerce-delivery/docker-compose.yml up -d
 echo Delivery Frontend has started
 
 
